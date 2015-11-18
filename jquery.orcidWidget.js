732c825af5f0d64cbd5db38156bb93bd8cc37c07
Michael Bar-Sinai <mich.barsinai@gmail.com>
2015-11-19 00:30:08 +0200

    Removed the title

--- jquery.orcidWidget.js
+++ jquery.orcidWidget.js
@@ -1,4 +1,4 @@
-var $jq = jQuery.noConflict()
+var $jq = jQuery.noConflict();
 $jq(document).ready(function() {
   var widget = $jq('#orcid-widget-js').clone(),
     orcids = widget.data(orcids).orcids.split(","),
@@ -6,22 +6,22 @@ $jq(document).ready(function() {
   $jq(orcids).each(function(name, value) {
     var profile_promise = get_orcid_profile(value.trim());
     profile_promise.success(function(data) {
-      profile = data['orcid-profile']
-      widget.append(set_widget_content(profile))
+      profile = data['orcid-profile'];
+      widget.append(set_widget_content(profile));
     });
   });
 
   $jq('#orcid-widget-js').replaceWith(widget);
 
   function set_widget_content() {
-    var profile_div = $jq('<div class="orcid-profile">')
-    set_person_works().appendTo(profile_div)
-    var orcid_uri = profile['orcid-identifier']['uri']
-    var orcid_link = $jq('<a class="orcid-profile-uri">')
-    orcid_link.attr("href", orcid_uri)
-    orcid_link.text("View full profile at ORCID")
-    orcid_link.appendTo(profile_div)
-    return profile_div
+    var profile_div = $jq('<div class="orcid-profile">');
+    set_person_works().appendTo(profile_div);
+    var orcid_uri = profile['orcid-identifier']['uri'];
+    var orcid_link = $jq('<a class="orcid-profile-uri">');
+    orcid_link.attr("href", orcid_uri);
+    orcid_link.text("View full profile at ORCID");
+    orcid_link.appendTo(profile_div);
+    return profile_div;
   }
 
   function get_orcid_profile(orcid) {
@@ -31,40 +31,39 @@ $jq(document).ready(function() {
       type: 'GET',
       dataType: 'jsonp',
       accepts: 'application/orcid+json'
-    })
+    });
   }
 
   function set_person_works() {
-    orcid_path = profile['orcid-identifier']['path']
-    var data = profile['orcid-activities']
+    orcid_path = profile['orcid-identifier']['path'];
+    var data = profile['orcid-activities'];
     var span = $jq('<span class="orcid-works">');
-    $jq('<h2>Works</h2>').appendTo(span);
     if (data['orcid-works']) {
       var works = data['orcid-works']['orcid-work'];
       span.append(list_person_works(works));
     } else {
       span.text("No works found.");
     }
-    return span
+    return span;
   }
 
   function list_person_works(works) {
     var seendois = [];
     var seentitles = [];
     var ul = $jq('<ul class="orcid-works">');
-    var i = 1
+    var i = 1;
     $jq(works).each(function(index, value) {
       var title = value['work-title']['title'].value;
       //Check for and ignore duplicate works based on title
-      if (jQuery.inArray(title, seentitles) == -1) {
+      if (jQuery.inArray(title, seentitles) === -1) {
         seentitles[seentitles.length] = title;
-        var ib = i % 2
-        if (ib == 0) {
+        var ib = i % 2;
+        if (ib === 0) {
           var li = $jq('<li class="orcid-work">');
         } else {
           var li = $jq('<li class="orcid-work orcid-work-odd">');
         }
-        i = i + 1
+        i = i + 1;
         var divtitle = $jq('<div class="orcid-work-title">');
         if ( value['url']['value'] ) {
             var link = $jq('<a>');
@@ -75,7 +74,7 @@ $jq(document).ready(function() {
           divtitle.text(title);
         }
 
-        var journal = value['journal-title'] != null ? value['journal-title'].value : "";
+        var journal = value['journal-title'] !== null ? value['journal-title'].value : "";
         var spanjournal = $jq('<span class="journal">');
         spanjournal.text(' ' + journal);
 
@@ -96,12 +95,12 @@ $jq(document).ready(function() {
         var author = "";
         var divauthors = $jq('<div class="work-authors">');
 
-        var contributors = value['work-contributors'] != null ? value['work-contributors']['contributor'] : "";
+        var contributors = value['work-contributors'] !== null ? value['work-contributors']['contributor'] : "";
         $jq(contributors).each(function(index, value) {
-          var author_orcid = value['contributor-orcid'] != null ? value['contributor-orcid']['path'] : "";
+          var author_orcid = value['contributor-orcid'] !== null ? value['contributor-orcid']['path'] : "";
 
           //combine the authors' names by span tag with different styles
-          if (author_orcid == orcid_path) {
+          if (author_orcid === orcid_path) {
             author = value['credit-name'].value;
             span_author = $jq('<span class="author">');
             span_author.text(author + '; ');
@@ -116,10 +115,10 @@ $jq(document).ready(function() {
           span_author.appendTo(divauthors);
         });
 
-        var journal = value['journal-title'] != null ? value['journal-title'].value : "";
-        var type = value['work-type'] != null ? value['work-type'] : "";
+        var journal = value['journal-title'] !== null ? value['journal-title'].value : "";
+        var type = value['work-type'] !== null ? value['work-type'] : "";
         var bibtex = value['work-citation']['work-citation-type'] === 'bibtex' ? value['work-citation']['citation'] : "";
-        var description = value['short-description'] != null ? value['short-description'] : "";
+        var description = value['short-description'] !== null ? value['short-description'] : "";
 
         divtitle.appendTo(li);
         divauthors.appendTo(li);
@@ -130,12 +129,12 @@ $jq(document).ready(function() {
           divWS.appendTo( li );
         }
 
-        var extids = value['work-external-identifiers'] != null ? value['work-external-identifiers']['work-external-identifier'] : "";
+        var extids = value['work-external-identifiers'] !== null ? value['work-external-identifiers']['work-external-identifier'] : "";
 
         var extIdUl = $jq('<ul class="orcid-external-ids">');
         $jq(extids).each(function(index, value) {
           var itemLi = $jq("<li>");
-          if (value['work-external-identifier-type'] == "DOI") {
+          if (value['work-external-identifier-type'] === "DOI") {
             var doi = "";
             var doilink = "http://dx.doi.org/";
             doi = value['work-external-identifier-id'].value.toUpperCase();
@@ -145,7 +144,7 @@ $jq(document).ready(function() {
             adoi.text("DOI");
             adoi.appendTo(itemLi);
 
-          } else if (value['work-external-identifier-type'] == "HANDLE") {
+          } else if (value['work-external-identifier-type'] === "HANDLE") {
             var handlelink = "http://hdl.handle.net/";
             var handle = value['work-external-identifier-id'].value;
             handlelink += handle;
@@ -154,7 +153,7 @@ $jq(document).ready(function() {
             ahandle.text("HDL");
             ahandle.appendTo(itemLi);
 
-          } else if (value['work-external-identifier-type'] == "ARXIV") {
+          } else if (value['work-external-identifier-type'] === "ARXIV") {
             handlelink = "http://arxiv.org/abs/";
             handle = value['work-external-identifier-id']['value'];
             handlelink += handle;
@@ -170,6 +169,6 @@ $jq(document).ready(function() {
 
       li.appendTo(ul);
     });
-    return ul
+    return ul;
   }
 });
